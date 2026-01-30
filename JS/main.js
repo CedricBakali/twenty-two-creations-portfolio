ced9c2d6e2c59abcbc6505a04c9e628820c06da0
CedricBakali <bakalicedric@gmail.com>
2026-01-30 05:11:54 +0200

    Worked on nav bar

--- JS/main.js
+++ JS/main.js
@@ -56,3 +56,43 @@ function revealOnScroll() {
 
 window.addEventListener("scroll", revealOnScroll);
 revealOnScroll();
+
+const sections = document.querySelectorAll("section[id]");
+const navLinks = document.querySelectorAll("nav a");
+
+function updateActiveNav() {
+    let currentSection = "";
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - 150;
+        const sectionHeight = section.offsetHeight;
+
+        if (window.scrollY >= sectionTop &&
+            window.scrollY < sectionTop + sectionHeight) {
+            currentSection = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+        if (link.getAttribute("href") === `#${currentSection}`) {
+            link.classList.add("active");
+        }
+    });
+}
+
+window.addEventListener("scroll", updateActiveNav);
+updateActiveNav();
+
+
+
+window.addEventListener("load", () => {
+    const loader = document.getElementById("loader");
+
+    loader.style.opacity = "0";
+    loader.style.transition = "opacity 0.6s ease";
+
+    setTimeout(() => {
+        loader.style.display = "none";
+    }, 600);
+});
